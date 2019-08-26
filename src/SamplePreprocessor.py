8a7c8239c5f032c4e25c208f7ebbb9cbc6b66336
jkpark <jaekeunprk@gmail.com>
2019-08-26 11:21:03 +0900

    data augmentation while training

--- src/SamplePreprocessor.py
+++ src/SamplePreprocessor.py
@@ -1,11 +1,18 @@
+import random
 import numpy as np
 import cv2
 
 
-def preprocess(img, imgsize):
+def preprocess(img, imgsize, dataAugmentation=False):
     # there are damaged files in iam dataset - just use black image instead
     if img is None:
-        img = np.zeros([1, 1])
+        img = np.zeros([imgSize[1], imgSize[0]])
+
+    # increase dataset size by applying random stretches to the images
+    if dataAugmentation:
+        stretch = (random.random() - 0.5) # -0.5 .. +0.5
+        wStretch = max(int(img.shape[1] * (1 + stretch)), 1) # random width, but at least 1
+        img = cv2.resize(img, (wStretch, img.shape[0])) # stretch horizontally by factor 0.5 .. 1.5
 
     # create target image and copy sample image into it
     (wt, ht) = imgsize
@@ -13,7 +20,7 @@ def preprocess(img, imgsize):
     fx = w / wt
     fy = h / ht
     f = max(fx, fy)
-    newsize = (min(wt, int(w / f)), min(ht, int(h / f)))
+    newsize = (max(min(wt, int(w / f)), 1), max(min(ht, int(h / f)), 1)) # scale according to f (result at least 1 and at most wt or ht
     img = cv2.resize(img, newsize)
     target = np.ones([ht, wt]) * 255
     target[0:newsize[1], 0:newsize[0]] = img
@@ -23,8 +30,8 @@ def preprocess(img, imgsize):
 
     # normalize
     (m, s) = cv2.meanStdDev(img)
-    m=m[0][0]
-    s=s[0][0]
+    m = m[0][0]
+    s = s[0][0]
     img = img - m
     img = img / s if s>0 else img
     return img
